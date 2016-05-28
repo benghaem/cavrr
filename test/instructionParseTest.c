3102fd471ac775595b11a7e24a8ff0d879e212db
Ben Ghaem <ben.ghaem@gmail.com>
2016-05-27 23:20:14 -0400

    add instruction parsing test

---
+++ test/instructionParseTest.c
@@ -0,0 +1,33 @@
+#include "../instruction.h"
+#include <stdio.h>
+#include "../util/intelhex.h"
+#include <unistd.h>
+
+int main(int argc, char** argv){
+
+    IHEX ih;
+    uint16_t op;
+    INSTRUCTION instr;
+
+    if (argc < 1){
+        printf("not enough arguments\n");
+    }
+
+    if(IHEX_open(&ih,argv[1])){
+        printf("Opened: %s\n", argv[1]);
+        while(!IHEX_at_end(&ih)){
+            op = (IHEX_get_byte(&ih) << 8) + IHEX_get_byte(&ih);
+            printf("%X\n",op);
+            instr = INSTRUCTION_decode_bytes(op);
+            INSTRUCTION_print(instr);
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
+
+
+
+
+
