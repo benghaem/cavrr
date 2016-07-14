2e1fd23e8c8c98f5f268d93526c42b03cc437054
Ben Ghaem <ben.ghaem@gmail.com>
2016-07-13 23:03:09 -0400

    correct makefile and built hex files

---
+++ test/hex/src/simpleoutput_plusdelay.c
@@ -0,0 +1,22 @@
+#include <avr/io.h>
+#include <avr/cpufunc.h>
+
+void delay(int cycles){
+    for (;cycles > 0; cycles--){
+        _NOP();
+    }
+}
+
+int main(void){
+
+    /* Enable output */
+    DDRB = 0xFF;
+
+    /* PORTB0 11110000 */
+    PORTB = 0xF0;
+
+    while(1){
+        PORTB = ~PORTB;
+        delay(200);
+    }
+}
